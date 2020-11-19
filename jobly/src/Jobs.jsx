a09806ea8df537d6a5f275f508e7a0baf17b3ba9
Omar6741 <70788409+Omar6741@users.noreply.github.com>
2020-11-18 18:58:33 -0600

    made Jobs.js somehwat functional

--- jobly/src/Jobs.jsx
+++ jobly/src/Jobs.jsx
@@ -1,15 +1,47 @@
-import React from "react";
-import { NavLink } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import JoblyApi from "./api";
+import JobCard from './JobCard';
+import Search from "./Search";
 
 
 
 function Jobs() {
+  const [jobs, setJobs] = useState(null);
+
+  useEffect(function getJobsOnMount() {
+    search();
+  }, []);
+
+  async function search(title) {
+    let result = await JoblyApi.getJobs(title);
+    setJobs(result);
+    console.log(result);  
+  }
+
+  if (!jobs) return "Loading....";
+  
   return (
-    <div className="App">
-      <h1>Available Job Listings</h1>
-      <h1><NavLink to="/"></NavLink></h1>
     
-    </div>
+      
+      <div>
+           <Search search={search} />
+        {jobs.length
+            ? (
+                <div>
+                  {jobs.map(j => (
+                      <JobCard
+
+                          company_handle={j.company_handle}
+                          title={j.title}
+                          salary={j.salary}
+                          equity={j.equity}
+                      />
+                  ))}
+                </div>
+            ) : (
+                <p className="lead">Sorry, no results were found!</p>
+            )}
+       </div>
   );
 }
 
